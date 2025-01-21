876622ebbd9d95a2345677a414f41dd0c36899a0
Aydan223 <8aydan.m@gmail.com>
2025-01-21 04:52:17 +0000

    small chat bot updates

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 print("Hello, and welcome. I am your informative ChatBot!")
 name = input("May I have your name please?: ")
 age = input("Nice to meet you " + name + ". May I also have your age?: ")
+print("Alright looks like we're set! Now if im correct you're " + age + ", and your name is " + name + ". Now, lets get started!")
+
+num = input("How can i be of help to you today?\n 1.|||\n 2.|||\n 3.|||\n 4. Exit ChatBot\n")
+
+if num == "4":
+    print("I'm glad that i could be of your assistance today. I'll see you again soon!")
+
