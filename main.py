f2047ed8a8cd2128da157678fddb6af40d5608e6
Aydan223 <8aydan.m@gmail.com>
2025-01-21 02:24:17 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,3 @@
+print("Hello, and welcome. I am your informative ChatBot!")
+name = input("May I have your name please?: ")
+age = input("Nice to meet you " + name + ". May I also have your age?: ")
